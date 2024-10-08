d19d623969025081147eb69152a4965253b7c478
Giovanni Seloto <giovanni.seloto.silva@gmail.com>
2024-10-07 22:32:28 -0300

    Atualização da tela index e criação de novos componentes

--- WakeMove/Components/imgIndex/styles.tsx
+++ WakeMove/Components/imgIndex/styles.tsx
@@ -5,7 +5,8 @@ const {width, height} = Dimensions.get('window')
 
 export const styles = StyleSheet.create({
     img:{
-        width: width * 0.1, 
-        height: height * 0.2, 
+        width: width * 0.4, 
+        height: height * 0.4, 
+        marginBottom: height * 0.05
     }
 })
--- WakeMove/app/_layout.tsx
+++ WakeMove/app/_layout.tsx
@@ -3,13 +3,15 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../Screens/';
 import CadastroScreen from '../Screens/Cadastro';
+import { StatusBar } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer independent={true}>  
-      <Stack.Navigator initialRouteName="Home">
+    <StatusBar barStyle="light-content" backgroundColor="#000000" />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Cadastro" component={CadastroScreen} />
       </Stack.Navigator>
