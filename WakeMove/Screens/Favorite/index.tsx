1d0d3710b6f91154678a268cde30f721653f864a
Giovanni Seloto <giovanni.seloto.silva@gmail.com>
2024-11-22 20:10:28 -0300

    Atualização de formulário

--- WakeMove/Screens/Favorite/index.tsx
+++ WakeMove/Screens/Favorite/index.tsx
@@ -1,29 +1,17 @@
-import React, { useState, useEffect } from "react";
-import AddFavorite from "@/Components/Organismo/ManageFavorites"; 
-import { CustomTitle } from "@/Components/Atomo/Title";
-import { Container } from "@/Components/Atomo/container";
-import { fetchFavorites } from "@/data/services/FetchFavorite"; 
-import { Favorite } from "@/data/userAuth/ViewDb";
-import { useAuth } from "@/data/userAuth/userCad"; // Importando o hook de autenticação
+import React from 'react';
+import FavoritesList from '@/data/services/viewFavorite';
+import { CustomTitle } from '@/Components/Atomo/Title';
+import { Container } from '@/Components/Atomo/container';
+import { Text } from 'react-native';
+import { useAuth } from '@/data/userAuth/userCad'; // Importando o hook de autenticação
 
 export const FavoriteScreen = () => {
   const { user } = useAuth(); // Pegando o usuário logado do hook
-  const [favorites, setFavorites] = useState<Favorite[]>([]);
-
-  // Função para buscar os favoritos quando a tela for carregada
-  useEffect(() => {
-    if (user) {
-      // Aqui você deve pegar o userId do usuário logado
-      fetchFavorites(user.uid, (favoritesData) => {
-        setFavorites(favoritesData); // Atualiza o estado com os favoritos
-      });
-    }
-  }, [user]); // Dependência em `user` para atualizar quando o usuário mudar
-
+  
   return (
     <Container>
-      <CustomTitle> Favoritos </CustomTitle>
-      <AddFavorite favorites={favorites} />
+      <CustomTitle>Favoritos</CustomTitle>
+      {user ? <FavoritesList /> : <Text> Carregando...</Text>}
     </Container>
   );
 };
--- WakeMove/data/services/FetchFavorite/index.tsx
+++ WakeMove/data/services/FetchFavorite/index.tsx
@@ -1,16 +1,10 @@
+import { viewFavorites } from "@/data/userAuth/ViewDb";
 import { Favorite } from "@/data/userAuth/ViewDb";
-import { viewFavorite } from "@/data/userAuth/ViewDb";
-
-// Tipagem do callback para receber um array de favoritos e retornar void
-export const fetchFavorites = (userId: string, callback: (favorites: Favorite[]) => void): void => {
-  if (!userId) {
-    console.error("Erro: userId não válido");
-    return;  // Não faz nada se o userId não for válido
-  }
-
+// Função que agora usa o userUniqueId
+export const fetchFavorites = (email: string, callback: (favorites: Favorite[]) => void): void => {
   try {
-    // Chama o viewFavorite que irá escutar as mudanças no Firestore
-    viewFavorite(userId, callback);  // Passa o callback para manipular os favoritos
+    // Chama a função que filtra os favoritos pelo userUniqueId
+    viewFavorites(email, callback);  // Passa o callback para manipular os favoritos
   } catch (error) {
     console.error("Erro ao buscar favoritos:", error);  // Tratamento de erro
   }
